7622c08eb7dd943808ac6dcf27b7371bfa98c82e
zJoyceLee <zJoyceLee@yeah.net>
2016-05-04 23:45:23 +0800

    Come true login, student_course html.

--- DataBase/courseSelectionBS/courseSelection/admin.py
+++ DataBase/courseSelectionBS/courseSelection/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-from courseSelection.models import S
-from courseSelection.models import C
-from courseSelection.models import SC
-
-admin.site.register(S)
-admin.site.register(C)
-admin.site.register(SC)
--- DataBase/courseSelectionBS/courseSelection/models.py
+++ DataBase/courseSelectionBS/courseSelection/models.py
@@ -1,62 +1,179 @@
+# ~/Code/Homework/DataBase/courseSelectionBS(branch:master*)
+# python manage.py inspectdb > ~/Code/Homework/DataBase/courseSelectionBS/courseSelection/models.py
+
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `# managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
-from django.core.validators import *
 
-
-class S(models.Model):
-    sno = models.CharField(max_length=4, validators=[MaxLengthValidator(4)])
-    sno.primary_key = True
-    sname = models.CharField(max_length=8, validators=[MaxLengthValidator(8)])
-    FEMALE, MALE = 'F', 'M'
-    sex_choice = (
-        (FEMALE, 'Female'),
-        (MALE, 'Male'),
-    )
-    sex = models.CharField(max_length=1,
-                           choices=sex_choice,
-                           default=MALE,
-                           validators=[RegexValidator(r'^[MF]$')]
-                           )
-    age = models.CharField(max_length=2, validators=[MaxLengthValidator(2)])
-    sdept = models.CharField(max_length=10, validators=[MaxLengthValidator(10)])
-    logn = models.CharField(max_length=20, validators=[MaxLengthValidator(20)])
-    pswd = models.CharField(max_length=20, validators=[MaxLengthValidator(20)])
-
-    def __str__(self):
-        return u"{0}-{1}".format(self.sno, self.sname)
-
-    def __unicode__(self):
-        return u"{0}-{1}".format(self.sno, self.sname)
-
-
-class C(models.Model):
-    cno = models.CharField(max_length=4, validators=[MaxLengthValidator(4)])
-    cno.primary_key = True
-    cname = models.CharField(max_length=20, validators=[MaxLengthValidator(20)])
-    credit = models.IntegerField(default=0, validators=[RegexValidator(r'^[46]$')])
-    cdept = models.CharField(max_length=20, validators=[MaxLengthValidator(20)])
-    tname = models.CharField(max_length=8, validators=[MaxLengthValidator(8)])
-
-    def __str__(self):
-        return u"{0}-{1}".format(self.cno, self.cname)
-
-    def __unicode__(self):
-        return u"{0}-{1}".format(self.cno, self.cname)
-
-
-class SC(models.Model):
-    sno = models.ForeignKey(S)
-    cno = models.ForeignKey(C)
-    grade = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
+class Colleges(models.Model):
+    name = models.CharField(primary_key=True, max_length=255)
 
     class Meta:
-        unique_together = ("cno", "sno")
+        # managed = False
+        db_table = 'Colleges'
 
-    def __str__(self):  # __unicode__ on Python 2
-        return u"{0}-{1}-{2}".format(self.sno, self.cno, self.grade)
+    def __str__(self):
+        return str(self.name)
 
-    def __unicode__(self):
-        return u"{0}-{1}-{2}".format(self.sno, self.cno, self.grade)
+
+class Courses(models.Model):
+    id = models.CharField(primary_key=True, max_length=8)
+    name = models.CharField(max_length=255)
+    college_name = models.ForeignKey(Colleges, models.DO_NOTHING, db_column='college_name', blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'Courses'
+
+
+class Opencourses(models.Model):
+    semester = models.CharField(max_length=255)
+    course = models.ForeignKey(Courses, models.DO_NOTHING)
+    teacher = models.ForeignKey('Teachers', models.DO_NOTHING)
+    class_time = models.CharField(max_length=255, blank=True, null=True)
+    rated = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'OpenCourses'
+        unique_together = (('course', 'teacher', 'semester'),)
+
+
+class Students(models.Model):
+    id = models.CharField(primary_key=True, max_length=8)
+    name = models.CharField(max_length=255)
+    college_name = models.ForeignKey(Colleges, models.DO_NOTHING, db_column='college_name', blank=True, null=True)
+    passwd = models.CharField(max_length=20)
+
+    class Meta:
+        # managed = False
+        db_table = 'Students'
+
+
+class Teachers(models.Model):
+    id = models.CharField(primary_key=True, max_length=8)
+    name = models.CharField(max_length=255)
+    college_name = models.ForeignKey(Colleges, models.DO_NOTHING, db_column='college_name')
+    passwd = models.CharField(max_length=20)
+
+    class Meta:
+        # managed = False
+        db_table = 'Teachers'
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=80)
+
+    class Meta:
+        # managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        # managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        # managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        # managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        # managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        # managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.SmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        # managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        # managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        # managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        # managed = False
+        db_table = 'django_session'
