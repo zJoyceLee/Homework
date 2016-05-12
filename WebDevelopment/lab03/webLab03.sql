51ef86d7b5e1a0f07e92b2653c3b9d2a08bbc766
zJoyceLee <zJoyceLee@yeah.net>
2016-05-13 02:45:13 +0800

    Rename info.sql.

---
+++ WebDevelopment/lab03/webLab03.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS webLab03;
+CREATE DATABASE webLab03 CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
+USE webLab03;
+
+CREATE TABLE User (
+    username VARCHAR(255) NOT NULL,
+    passwd VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    birthday VARCHAR(255) NOT NULL,
+    birthplace  VARCHAR(255) NOT NULL,
+    gender VARCHAR(8) DEFAULT 'Male',
+    hobby VARCHAR(255) NOT NULL,
+    info VARCHAR(1024) NOT NULL,
+
+    PRIMARY KEY(userName)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
