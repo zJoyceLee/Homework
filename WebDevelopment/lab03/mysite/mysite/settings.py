4fb0e5be9dfd6f55da8082b2ab3307a9d3d9166e
zJoyceLee <zJoyceLee@yeah.net>
2016-05-13 02:39:34 +0800

    Try to add argon2, finally didn't do it.

--- WebDevelopment/lab03/mysite/mysite/settings.py
+++ WebDevelopment/lab03/mysite/mysite/settings.py
@@ -122,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# PASSWORD_HASHERS = [
+#     'django.contrib.auth.hashers.Argon2PasswordHasher',
+# ]
