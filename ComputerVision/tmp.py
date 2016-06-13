8c8e042a36604b5b92879e9e90fa6cd98738715c
zJoyceLee <zJoyceLee@yeah.net>
2016-06-13 19:36:21 +0800

    Force Value on part7: ValueError: could not broadcast input array from shape (445,78,3) into shape (445,77,3).

--- ComputerVision/tmp.py
+++ ComputerVision/tmp.py
@@ -3,22 +3,22 @@ import numpy as np
 
 img = cv2.imread('./2.jpg')
 
-# part1 = img[175:620, 73:146]
-# part2 = img[175:620, 182:250]
-# part3 = img[175:620, 262:362]
-# part4 = img[175:620, 400:486]
-# part5 = img[175:620, 505:572]
-# part6 = img[175:620, 604:675]
-# part7 = img[175:620, 712:790]
-# part8 = img[175:620, 818:893]
-# cv2.imwrite('./lab03Img/part1.jpg', part1)
-# cv2.imwrite('./lab03Img/part2.jpg', part2)
-# cv2.imwrite('./lab03Img/part3.jpg', part3)
-# cv2.imwrite('./lab03Img/part4.jpg', part4)
-# cv2.imwrite('./lab03Img/part5.jpg', part5)
-# cv2.imwrite('./lab03Img/part6.jpg', part6)
-# cv2.imwrite('./lab03Img/part7.jpg', part7)
-# cv2.imwrite('./lab03Img/part8.jpg', part8)
+part1 = img[175:620, 73:146]
+part2 = img[175:620, 182:250]
+part3 = img[175:620, 262:362]
+part4 = img[175:620, 400:486]
+part5 = img[175:620, 505:572]
+part6 = img[175:620, 604:675]
+part7 = img[175:620, 712:790]
+part8 = img[175:620, 818:893]
+cv2.imwrite('./lab03Img/part1.jpg', part1)
+cv2.imwrite('./lab03Img/part2.jpg', part2)
+cv2.imwrite('./lab03Img/part3.jpg', part3)
+cv2.imwrite('./lab03Img/part4.jpg', part4)
+cv2.imwrite('./lab03Img/part5.jpg', part5)
+cv2.imwrite('./lab03Img/part6.jpg', part6)
+cv2.imwrite('./lab03Img/part7.jpg', part7)
+cv2.imwrite('./lab03Img/part8.jpg', part8)
 
 part1 = cv2.imread('./lab03Img/part1.jpg')
 part2 = cv2.imread('./lab03Img/part2.jpg')
@@ -27,6 +27,8 @@ part4 = cv2.imread('./lab03Img/part4.jpg')
 part5 = cv2.imread('./lab03Img/part5.jpg')
 part6 = cv2.imread('./lab03Img/part6.jpg')
 part7 = cv2.imread('./lab03Img/part7.jpg')
+row, col, channel = part7.shape
+print(row, col, channel)
 part8 = cv2.imread('./lab03Img/part8.jpg')
 
 result = img[175:620, 0:620]
@@ -38,7 +40,7 @@ result[0:445, 140:215] = part8
 result[0:445, 215:315] = part3
 result[0:445, 315:386] = part6
 result[0:445, 386:472] = part4
-result[0:445, 472:552] = part7
+result[0:445, 472:550] = part7[0:455, 0:78]
 result[0:445, 552:620] = part2
 
 
