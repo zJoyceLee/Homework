8be79e10940e3532947de4ba4055402e6a07c228
zJoyceLee <zJoyceLee@yeah.net>
2016-07-08 15:05:27 +0800

    change url.

--- ProductionPractice/mysite/online/urls.py
+++ ProductionPractice/mysite/online/urls.py
@@ -7,11 +7,9 @@ urlpatterns = [
     url(r'^$', views.login, name='login'),
     url(r'^login/$',views.login,name = 'login'),
     url(r'^regist/$',views.regist,name = 'regist'),
-    url(r'^index/$',views.index,name = 'index'),
     url(r'^commodity/$', views.commodity, name = 'commodity'),
     url(r'^add_commodity/$', views.add_commodity, name = 'add_commodity'),
     url(r'^logout/$',views.logout,name = 'logout'),
     url(r'^get_user_info/$', views.get_user_info, name = 'get_user_info'),
     url(r'^captcha/$', views.captcha, name = 'captcha'),
-    url(r'^update_info/$', views.update_info, name = 'update_info'),
 ]
