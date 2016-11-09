f21a913a366d4b2f69ad6cc6678283513e75e485
zJoyceLee <zJoyceLee@yeah.net>
2016-11-09 08:45:18 +0800

    pl/sql.

--- Oracle/06_hw.sql
+++ Oracle/06_hw.sql
@@ -1,24 +1,29 @@
-SYSTEM echo '对基本表S、C和SC操作：';
-SYSTEM echo '1．用PL/SQL的存储过程删除学号为S8的学生。';
-DECLARE
+INSERT INTO S(s#, sname, age, sex) VALUES ('s8', 'dong', 18, 'F');
+SELECT s#, sname FROM S WHERE s# = 's8';
+CREATE OR REPLACE PROCEDURE delStu (val IN S.s#%type)
+IS
 BEGIN
-    DELETE FROM S WHERE s# = 's8';
-END;
+    DELETE FROM S WHERE s# = val;
+END delStu;
+/
+EXECUTE delStu('s8');
+SELECT s#, sname FROM S WHERE s# = 's8';
 
-SYSTEM echo '2．用带输入输出参数的存储过程查询出任意给定学号和课程后的成绩。';
+
+SELECT * FROM SC;
 CREATE OR REPLACE PROCEDURE query_grade (
     sno IN SC.s#%type,
     cno IN SC.c#%type,
     out_grade OUT SC.grade%type
 ) IS
 BEGIN
-    SELECT grade FROM SC WHERE s# = sno AND c# = cno;
+    SELECT grade INTO out_grade FROM SC WHERE s# = sno AND c# = cno;
 END query_grade;
-VARIABLE sc_grade NUMBER(3);
-EXECUTE query_grade('s1', 'c1', :out_grade);
-PRINT out_grade;
+/
+VARIABLE sc_grade NUMBER;
+EXECUTE query_grade('s1', 'c1', :sc_grade);
+PRINT sc_grade;
 
-SYSTEM echo '3．用函数做第2题。';
 CREATE OR REPLACE FUNCTION Fun_query_grade (
     sno IN SC.s#%type,
     cno IN SC.c#%type
@@ -29,6 +34,7 @@ BEGIN
     SELECT grade INTO val_grade FROM SC WHERE s# = sno AND c# = cno;
     RETURN (val_grade);
 END Fun_query_grade;
-VARIABLE sc_grade NUMBER(3);
-EXECUTE :sc_grade := Fun_query_grade('s1', 'c1');
-PRINT sc_grade;
+/
+VARIABLE f_sc_grade NUMBER;
+EXECUTE :f_sc_grade := Fun_query_grade('s1', 'c1');
+PRINT f_sc_grade;
--- Oracle/06_lab.sql
+++ Oracle/06_lab.sql
@@ -1,32 +1,46 @@
-SYSTEM e cho '1．在SQL*Plus中使用PL/SQL块处理';
-SYSTEM echo 'EMP表中职工号7788的职工，如果工资小于3000那么把工资更改为3000：';
+SELECT empno, ename, sal FROM EMP WHERE empno = 7788;
+UPDATE EMP SET sal = 200 WHERE empno = 7788;
+SELECT empno, ename, sal FROM EMP WHERE empno = 7788;
 DECLARE
-x NUMBER(7, 2)
+x NUMBER(7, 2);
 BEGIN
     SELECT sal INTO x FROM EMP WHERE empno = 7788;
     IF x < 3000 THEN
         UPDATE EMP SET sal = 3000 WHERE empno = 7788;
         END IF;
 END;
+/
+SELECT empno, ename, sal FROM EMP WHERE empno = 7788;
 
-SYSTEM echo '2．无参数的存储过程';
+
+
+SELECT empno, ename, sal FROM EMP WHERE empno = 9010;
 CREATE OR REPLACE PROCEDURE proc_execution
+IS
 BEGIN
     UPDATE EMP SET ename = 'yourname' WHERE empno = 9010;
 END proc_execution;
+/
 EXECUTE proc_execution;
+SELECT empno, ename, sal FROM EMP WHERE empno = 9010;
 
-SYSTEM echo '3．带输入参数的存储过程：';
-SYSTEM echo '解雇给定职工号的职工，并调用proc_execution, 存储过程删除了职工号7654的职工';
+
+
+SELECT empno, ename, sal FROM EMP ORDER BY 1;
 CREATE OR REPLACE PROCEDURE fire_emp (val IN EMP.empno%type)
 IS
 BEGIN
     proc_execution;
     DELETE FROM EMP WHERE empno = val;
 END fire_emp;
-EXECUTE fire_emp;
+/
+EXECUTE fire_emp(7654);
+SELECT empno, ename, sal FROM EMP ORDER BY 1;
 
-SYSTEM echo '4．带输入输出的存储过程, 查询EMP中给定职工号的姓名、工资和佣金。';
+INSERT INTO EMP(empno, ename, job, mgr, hiredate, sal, comm, deptno) VALUES
+(7654, 'martin', 'salesman', 7698, TO_DATE('1981-09-28', 'yyyy-mm-dd'), 1250, 1400, 30);
+
+
 CREATE OR REPLACE PROCEDURE query_emp (
     val_empno IN EMP.empno%type,
     val_ename OUT EMP.ename%type,
@@ -37,13 +51,13 @@ BEGIN
     SELECT ename, sal, comm INTO val_ename, val_sal, val_comm
     FROM EMP WHERE empno = val_empno;
 END query_emp;
+/
 VARIABLE emp_name VARCHAR2(15);
 VARIABLE emp_sal NUMBER;
 VARIABLE emp_comm NUMBER;
 EXECUTE query_emp(7654, :emp_name, :emp_sal, :emp_comm);
 PRINT emp_name;
 
-SYSTEM echo '5．用Function查询出EMP中给定职工号的工资：';
 CREATE OR REPLACE FUNCTION get_sal (val_empno IN EMP.empno%type)
 RETURN NUMBER
 IS
@@ -52,13 +66,11 @@ BEGIN
     SELECT sal INTO val_sal FROM EMP WHERE empno = val_empno;
     RETURN (val_sal);
 END get_sal;
+/
 VARIABLE emp_sal NUMBER;
 EXECUTE :emp_sal := get_sal(7654)
 PRINT emp_sal;
 
-SYSTEM echo '6．用异常处理完善程';
-SYSTEM echo '如例3中：解雇给定职工号的职工，并调用proc_execution';
-SYSTEM echo '如果职工号7654的职工不存在则出错。为了避免出错我们使用了EXCEPTION语句。';
 CREATE OR REPLACE PROCEDURE fire_emp (val_empno IN EMP.empno%type)
 IS
 BEGIN
@@ -68,3 +80,4 @@ BEGIN
         RAISE_APPLICATION_ERROR(-20202, 'Employee does not exists.');
     END IF;
 END fire_emp;
+/
