e2996e742e6aecb9168ac088827dc0be3874bb0f
zJoyceLee <zJoyceLee@yeah.net>
2017-03-17 02:02:17 +0800

    add class:'downloader', slice picture.

--- GraduationProject/downloadApp/test.py
+++ GraduationProject/downloadApp/test.py
@@ -1,12 +1,40 @@
 import requests
 import json
 
+class downloader:
+    def __init__(self, myurl, myname):
+        self.url = myurl
+        self.thread_num = 8
+        self.name = '{}{}'.format(myname, myurl[myurl.rfind('.'):])
+        r = requests.head(self.url)
+        self.total = int(r.headers['Content-length'])
+        print('{}\ntotal is {}.'.format(self.url, self.total))
+    def get_range(self):
+        ranges = []
+        offset = int(self.total / self.thread_num)
+        for i in range(self.thread_num):
+            if i == self.thread_num - 1:
+                ranges.append((i*offset, ''))
+            else:
+                ranges.append((i*offset,  (i+1)*offset))
+        return ranges
+    def run(self, dest):
+        with open('{}/{}'.format(dest, self.name), 'bw') as f:
+            for piece  in self.get_range():
+                res =  requests.get(
+                    self.url,
+                    headers={
+                        'Range': 'Bytes={}-{}'.format(piece[0], piece[1]),
+                        'Accept-Encoding': '*'
+                    }
+                )
+                f.seek(piece[0])
+                f.write(res.content)
+
+
+
 with open('./source/url.json', 'r') as src:
     d = json.load(src)
-
     for key, value  in d.items():
-        h = {'Range': 'Bytes=0-15000', 'Accpet-Encoding': '*'}
-
-        res = requests.get('{}'.format(value), headers=h)
-        with open('./download/{}.jpg'.format(key), 'bw') as  f:
-            f.write(res.content)
+        down = downloader(key, value)
+        down.run('download')
