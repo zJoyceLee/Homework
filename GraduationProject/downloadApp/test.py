c8058fa6792ce24eddf3edfaacbb62c8cb154fbd
zJoyceLee <zJoyceLee@yeah.net>
2017-03-17 01:09:48 +0800

    add headers

--- GraduationProject/downloadApp/test.py
+++ GraduationProject/downloadApp/test.py
@@ -5,6 +5,8 @@ with open('./source/url.json', 'r') as src:
     d = json.load(src)
 
     for key, value  in d.items():
-        res = requests.get('{}'.format(value))
+        h = {'Range': 'Bytes=0-15000', 'Accpet-Encoding': '*'}
+
+        res = requests.get('{}'.format(value), headers=h)
         with open('./download/{}.jpg'.format(key), 'bw') as  f:
             f.write(res.content)
