4142486e66c6ff2cf51e6f940fa4fd102399a3ec
zJoyceLee <zJoyceLee@yeah.net>
2017-03-20 23:57:58 +0800

    add log.

--- GraduationProject/downloadApp/download.py
+++ GraduationProject/downloadApp/download.py
@@ -3,9 +3,11 @@ import sys
 
 try:
     url  = sys.argv[1]
-    dest = sys.argv[2]
+    path = sys.argv[2]
+    print('url: {}'.format(url))
+    print('path: {}'.format(path))
     res = requests.get(url)
-    with open('{}'.format(sys.argv[2]), 'bw') as f:
+    with open('{}'.format(path), 'bw') as f:
         f.write(res.content)
 except:
     print('USAGE: python download.py [download-url] [save-path]')
