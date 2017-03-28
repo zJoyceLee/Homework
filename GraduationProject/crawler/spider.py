1eb19a7f997eb0e46a2ee5ec9065e318792a0304
zJoyceLee <zJoyceLee@yeah.net>
2017-03-29 00:31:34 +0800

    impl a class named Spider.

--- GraduationProject/crawler/spider.py
+++ GraduationProject/crawler/spider.py
@@ -4,22 +4,35 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
-def get_a_href_title(tag):
-    return tag.has_attr('href') and tag.has_attr('title')
+class Spider():
+    def __init__(self):
+        self.url = ''
+        try:
+            self.url = sys.argv[1]
+        except:
+            self.url = 'http://blog.jobbole.com/'
+            print('[USAGE]:  python spider.py [url]')
+            print('Default url: {}'.format(self.url))
+            time.sleep(3)
 
-url =  'http://blog.jobbole.com/'
-try:
-    url = sys.argv[1]
-except:
-    print('[USAGE]:  python spider.py [url]')
-    print('Default url: {}'.format(url))
-    time.sleep(3)
 
-print(url)
-res = requests.get(url)
+    def get_a_href_title(tag):
+        return tag.tag.has_attr('href') and tag.has_attr('title')
 
-soup = BeautifulSoup(res.text)
-# print(soup.prettify())
-content_lst = soup.find_all(get_a_href_title)
+    def parse(self):
+        res = requests.get(self.url)
+        soup = BeautifulSoup(res.content)
+        content_lst = soup.find_all('a')
+        print('**********C-ontent**********')
+        [print('''--link--{}
+                   \n--title--{}
+                   \n--text--{}\n\n'''
+               .format(
+                   x.get('href'),
+                   x.get('title'),
+                   x.get_text())
+               ) for x in content_lst]
+        print('**********Content**********')
 
-[print('href: {}\ntitle: {}\ntext: {}\n'.format(x.get('href'), x.get('title'),x.get_text())) for x in content_lst]
+s = Spider()
+s.parse()
