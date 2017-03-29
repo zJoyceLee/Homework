7c0df9c1f7d71e118ae17f862c9869aeb022d585
zJoyceLee <zJoyceLee@yeah.net>
2017-03-29 23:34:16 +0800

    find a serie of video in Youku.

--- GraduationProject/crawler/spider.py
+++ GraduationProject/crawler/spider.py
@@ -4,34 +4,32 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
-class Spider():
+class Spider(object):
     def __init__(self):
         self.url = ''
         try:
             self.url = sys.argv[1]
         except:
-            self.url = 'http://blog.jobbole.com/'
+            # self.url = 'http://blog.jobbole.com/'
+            self.url = "http://v.youku.com/v_show/id_XMjYxMDUzNjg1Mg==.html"
             print('[USAGE]:  python spider.py [url]')
             print('Default url: {}'.format(self.url))
             time.sleep(3)
 
+    def has_href_and_title(self, tag):
+        return tag.has_attr('href') and tag.has_attr('title')
 
-    def get_a_href_title(tag):
-        return tag.tag.has_attr('href') and tag.has_attr('title')
+    def has_href_and_class(self, tag):
+        return  tag.has_attr('href') and tag.has_attr('class')
 
     def parse(self):
         res = requests.get(self.url)
         soup = BeautifulSoup(res.content)
-        content_lst = soup.find_all('a')
+        content_lst = soup.find_all(self.has_href_and_title)
+        content_lst = soup.find_all(attrs={"class": "sn"})
         print('**********C-ontent**********')
-        [print('''--link--{}
-                   \n--title--{}
-                   \n--text--{}\n\n'''
-               .format(
-                   x.get('href'),
-                   x.get('title'),
-                   x.get_text())
-               ) for x in content_lst]
+        [print(x.get('href')) for x in content_lst]
+        print('total: {}'.format(len(content_lst)))
         print('**********Content**********')
 
 s = Spider()
